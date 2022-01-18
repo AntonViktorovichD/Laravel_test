5fc27bc518a89b435f36fac0110a2dad2d35eaee
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-01-18 13:27:53 +0300

    Добавлена тестовая БД

---
+++ test.sql
@@ -0,0 +1,207 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Янв 18 2022 г., 10:27
+-- Версия сервера: 8.0.27
+-- Версия PHP: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+CREATE TABLE IF NOT EXISTS `cities` (
+  `city_id` int NOT NULL AUTO_INCREMENT,
+  `city_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `city_country_id` tinyint NOT NULL,
+  PRIMARY KEY (`city_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `cities`
+--
+
+INSERT INTO `cities` (`city_id`, `city_name`, `city_country_id`) VALUES
+(1, 'Минск', 1),
+(2, 'Витебск', 1),
+(3, 'Москва', 2),
+(4, 'Питер', 2),
+(5, 'Лондон', 0),
+(6, 'Киев', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `countries`
+--
+
+DROP TABLE IF EXISTS `countries`;
+CREATE TABLE IF NOT EXISTS `countries` (
+  `country_id` int NOT NULL AUTO_INCREMENT,
+  `country_name` varchar(16) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`country_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `countries`
+--
+
+INSERT INTO `countries` (`country_id`, `country_name`) VALUES
+(1, 'Беларусь'),
+(2, 'Россия'),
+(3, 'Украина');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `employees`
+--
+
+DROP TABLE IF EXISTS `employees`;
+CREATE TABLE IF NOT EXISTS `employees` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthday` date NOT NULL,
+  `position` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `salary` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `employees`
+--
+
+INSERT INTO `employees` (`id`, `name`, `birthday`, `position`, `salary`) VALUES
+(1, 'Дима', '1988-03-25', 'дизайнер', '400'),
+(2, 'Петя', '1989-03-26', 'дизайнер', '500'),
+(3, 'Вася', '1990-05-26', 'верстальщик', '700'),
+(4, 'Коля', '1990-03-25', 'верстальщик', '1000'),
+(5, 'Иван', '1991-05-27', 'программист', '600'),
+(6, 'Кирилл', '1990-06-27', 'программист', '2000');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+CREATE TABLE IF NOT EXISTS `events` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start` date NOT NULL,
+  `finish` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `events`
+--
+
+INSERT INTO `events` (`id`, `name`, `start`, `finish`) VALUES
+(1, 'Мероприятие 1', '2020-03-25', '2020-03-26'),
+(2, 'Мероприятие 2', '2020-04-21', '2020-04-21'),
+(3, 'Мероприятие 3', '2020-05-23', '2020-05-23'),
+(4, 'Мероприятие 4', '2020-06-23', '2020-06-28');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `title` char(200) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `desc` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `text` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `desc`, `text`, `date`) VALUES
+(1, 'Первая статья', 'Первая', '<p>Первая статья.</p>', '2011-09-01'),
+(2, 'Вторая статья', 'Вторая', '<p>Вот и вторая статья</p>', '2011-09-08'),
+(3, 'Третья статья', 'Третья', '<p>Третья статья.</p>', '2011-09-09'),
+(4, 'Четвертая статья', 'Четвертая', '<p>Да, это четвертая статья.</p>', '2011-09-20'),
+(5, 'Пятая статья', 'Пятая', '<p>Моя пятая статья</p>', '2021-12-27'),
+(6, 'Шестая статья', 'Шестая', '<p>Моя очередная шестая статья</p>', '2022-01-19');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `login` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(16) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(24) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `password`, `email`) VALUES
+(2, 'dayle', '123654', 'dayle@example.com'),
+(3, 'lexa', '123654', 'lexa@example.com'),
+(4, 'axel', '123654', 'axel@example.com');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `surname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `age` tinyint NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `surname`, `age`) VALUES
+(1, 'user1', 'surname1', 20),
+(2, 'user2', 'surname2', 25),
+(3, 'user3', 'surname3', 28),
+(4, 'user4', 'surname4', 28),
+(5, 'user5', 'surname5', 27),
+(6, 'user6', 'surname6', 30);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
