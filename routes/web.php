fd6bef754cb0e232a4df83d0599d5073d62d2f4e
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-01-19 09:15:18 +0300

    Добавлена проверка регулярным выражением маршрутов

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-Route::get('/test/user/{id}', 'PostController@getOne');
-Route::get('/test/user/', 'PostController@getAll');
+Route::get('/test/user/{id}', 'PostController@getOne')->where('id', '[0-9]+');;
+Route::get('/test/user/all', 'PostController@getAll');
 Route::match(['get', 'post'], '/test/new', 'PostController@newPost');
 Route::post('/test/result', 'PostController@newPost');
 Route::match(['get', 'post'], 'test/edit/{id}', 'PostController@editPost');
